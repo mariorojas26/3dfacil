cf5219a2867e18db95665292b06eeeb8227a1df9
mariorojas26 <ingenieromarior@gmail.com>
2025-05-20 16:30:38 -0500

    NuevoBannerPrincipal

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,11 @@ $(function() {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".header_navbar").removeClass("sticky");
-            $(".header_navbar img").attr("src", "assets/images/logo.svg");
+            $(".header_navbar img").attr("src", "assets/images/logo3d.png");
         } else {
             $(".header_navbar").addClass("sticky");
-            $(".header_navbar img").attr("src", "assets/images/logo-2.svg");
+            $(".header_navbar img").attr("src", "assets/images/logo3d.png");
+            // logo de otro color
         }
     });
     
